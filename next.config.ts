ef21ecbf4e283562b4d0c01de6efd8610fa7a841
CloudPremises <cloudpremises0@gmail.com>
2024-12-19 17:39:22 -0400

    policies

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: '/iccmw-web-app',
+  assetPrefix: '/iccmw-web-app/',
+  
   /* config options here */
   images: {
     remotePatterns: [
